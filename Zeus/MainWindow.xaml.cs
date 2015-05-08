05222700d6f9cdd990f11f99592b3f0dfaf3f996
Ilya Minyaev <ayli.veaynim@gmail.com>
2015-05-09 00:32:43 +0300

    Interface update

--- Zeus/MainWindow.xaml.cs
+++ Zeus/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace Zeus
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow() {
@@ -35,6 +36,18 @@ namespace Zeus
             if (openFileDialog.ShowDialog() == true) {
                 System.Diagnostics.Debug.WriteLine(openFileDialog.FileName);
                 LogManager.Session.logMessage("Opening " + openFileDialog.FileName + " input file");
+                Engine.Engine.Instance.initSphereWithInputFile(openFileDialog.FileName);
+                setInformation();
+            }
+        }
+
+        private void setInformation() {
+            double latitude = Engine.Engine.Instance.lowAtmosphere.latitude;
+            double longitude = Engine.Engine.Instance.lowAtmosphere.longitude;
+            longitudeTextBox.Text = longitude.ToString();
+            latitudeTextBox.Text = latitude.ToString();
+            foreach (Element el in Engine.Engine.Instance.lowAtmosphere.aerosolElements) {
+                elementsPanel.Text += String.Format("{0} = {1}\n", el.name, el.n0);
             }
         }
     }
